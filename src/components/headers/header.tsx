e91b30ebd3583a7396e9e4f760851dad8fbb008f
AbdulRehmanjr <abdulrehman2020white@gmail.com>
2024-10-17 19:58:49 +0500

    fix: tailwindcss lecture 1

--- src/components/headers/header.tsx
+++ src/components/headers/header.tsx
@@ -6,17 +6,17 @@ export const Header = () => {
 
     return (
         <header className="grid">
-            <nav className="grid grid-cols-12 place-items-center gap-8 bg-green-400 p-4 text-white text-xs lg:text-base">
+            <nav className="grid grid-cols-12 place-items-center gap-8 bg-green-400 p-3 text-white text-xs lg:text-base">
                 <div className="col-span-4 lg:col-span-3 flex items-center gap-3">
-                    <Phone className='h-4 w-4'/>
+                    <Phone className='h-4 w-4 lg:h-5 lg:w-5'/>
                     <p >+92 330 14556561</p>
                 </div>
                 <div className="col-span-4 lg:col-span-3 flex items-center gap-3">
-                    <Mail className='h-4 w-4'/>
+                    <Mail className='h-4 w-4 lg:h-5 lg:w-5'/>
                     <p>example@gmail.com</p>
                 </div>
                 <div className="col-span-4 lg:col-span-3 flex items-center gap-3">
-                    <Clock className='h-4 w-4'/>
+                    <Clock className='h-4 w-4 lg:h-5 lg:w-5'/>
                     <p>Mon to Friday: 9am - 5pm</p>
                 </div>
                 <div className='col-span-12 lg:col-span-3 flex gap-2'>
@@ -36,7 +36,15 @@ export const Header = () => {
                 </div>
             </nav>
             <div>
-                
+                <div>
+
+                </div>
+                <div>
+
+                </div>
+                <div>
+                    
+                </div>
             </div>
         </header>
     )
